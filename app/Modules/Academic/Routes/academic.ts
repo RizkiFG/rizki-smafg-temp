eb78ae7c4ef3c96ea57f26cbad5b29f983167eb3
Rizki-SMAFG <rizki.it@smafg.sch.id>
2023-05-16 14:11:18 +0700

    initial commit

---
+++ app/Modules/Academic/Routes/academic.ts
@@ -0,0 +1,9 @@
+import Route from '@ioc:Adonis/Core/Route'
+Route.group(() => {
+    Route.get('/', () => {
+        return "You got here at academic"
+    })
+    Route.resource('classes', 'ClassesController').apiOnly().middleware({ '*': ['auth'] })
+    Route.resource('students', 'StudentsController').apiOnly().middleware({ '*': ['auth'] })
+    Route.resource('subjects', 'SubjectsController').apiOnly().middleware({ '*': ['auth'] })
+}).prefix('academics').namespace('AcademicControllers')
